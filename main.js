4804b1041686154a1643ebc515d3caf0a8e8e071
Evan Leri <m.evanleri@gmail.com>
2017-11-09 14:49:31 -0500

    EvanL macbook

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function checkTranslation (id, answer) {
   console.log(id);
-  if (document.getElementById(id).value.toLowerCase() == answer.toLowerCase())  window.location.href = 'Correct.html'; else alert('Try again!');
+	if (document.getElementById(id).value.toLowerCase() == answer.toLowerCase()) {alert('Good Job!')} else {alert('Try again!')}
+ // if (document.getElementById(id).value.toLowerCase() == answer.toLowerCase())  window.location.href = 'Correct.html'; else alert('Try again!');
 }
