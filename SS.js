935ffbb0189b6b3537f95750f27a737456aeee7b
Evan Leri <m.evanleri@gmail.com>
2017-11-11 17:58:43 -0500

    ElMbH

--- SS.js
+++ SS.js
@@ -6,3 +6,11 @@ function unhide() {
         x.style.display = "none";
     }
 }
+function sesame(pass) {
+    var s = document.getElementById('openses')
+    var ss = document.getElementById('sss')
+  if( document.getElementById(Password).value.toLowerCase() === pass.toLowerCase()) {
+    s.style.display = "none";
+    ss.style.display = "block";
+  }
+}
