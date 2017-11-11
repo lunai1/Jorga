ca3811ef7b89ff3ceec5af7940c0242e36137c34
Evan Leri <m.evanleri@gmail.com>
2017-11-11 17:37:07 -0500

    ELMBH

--- SS.js
+++ SS.js
@@ -1,7 +1,7 @@
 function unhide() {
   var x = document.getElementById("hidden");
     if (x.style.display === "none") {
-        x.style.display = "block";
+        x.style.display = "inline";
     } else {
         x.style.display = "none";
     }
